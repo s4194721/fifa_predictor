197673b4008e1d7cef1474d579f65bd7419acea3
s4194721 <s4194721@student.rmit.edu.au>
2026-07-05 13:15:46 +0600

    db

--- db.php
+++ db.php
@@ -1,14 +1,21 @@
 <?php
-$host = "sql207.infinityfree.com";
-$dbname = "if0_42334064_epiz_12345678_fifa";
-$username = "if0_42334064";
-$password = "nanjiba2862005";
-
-$admin_password = 'admin2026';
+$host = getenv("mysql.railway.internal");
+$dbname = getenv("railway");
+$username = getenv("root");
+$password = getenv("vLTadGddYNzcqJwotvEbjeItAvGGZjXl");
+$port = getenv("3306");
 
 try {
-    $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $username, $password);
+    $pdo = new PDO(
+        "mysql:host=$host;port=$port;dbname=$dbname;charset=utf8mb4",
+        $username,
+        $password
+    );
+
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
-} catch (PDOException $e) {
-    die('Database connection failed: ' . htmlspecialchars($e->getMessage()));
+
+} catch(PDOException $e) {
+    die("Database connection failed: " . htmlspecialchars($e->getMessage()));
+}
+?>
